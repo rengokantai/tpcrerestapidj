f413f255c8a88698a48bd9cdab83d5d37a3c2fe7
Hernan Y.Ke <yuriqiao@gmail.com>
2017-01-12 18:57:28 -0500

    L2_5

--- tpcrerestapidj/invoices/views.py
+++ tpcrerestapidj/invoices/views.py
@@ -7,12 +7,18 @@ from invoices.models import Invoice
 from invoices.serializers import InvoiceSerializer
 from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def invoice_list(request):
     if request.method=='GET':
         invoices = Invoice.objects.all()
         serializer = InvoiceSerializer(invoices,many=True)
         return Response(serializer.data)
+    elif request.method =='POST':
+        serializer = InvoiceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 @api_view(['GET'])
 def invoice_detail(request,pk):
     try:
