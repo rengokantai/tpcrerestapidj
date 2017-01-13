4659a2f689e5cc9282d648f66aca60ae50526455
Hernan Y.Ke <yuriqiao@gmail.com>
2017-01-12 19:21:20 -0500

    L2_6

--- tpcrerestapidj/invoices/views.py
+++ tpcrerestapidj/invoices/views.py
@@ -19,11 +19,21 @@ def invoice_list(request):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def invoice_detail(request,pk):
     try:
         invoice = Invoice.objects.get(pk=pk)
     except Invoice.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method =='GET':
+        serializer = InvoiceSerializer(invoice)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = InvoiceSerializer(invoice,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
     serializer = InvoiceSerializer(invoice)
     return Response(serializer.data)
